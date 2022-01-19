40ea9aed9679a16f7875527281b237cdaffba57b
120914 <splie30@hotmail.com>
2022-01-19 01:39:45 +0100

    changed cnn architecture of cnn.py

--- CNN.py
+++ CNN.py
@@ -8,18 +8,24 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5, stride=2)
+        self.conv1 = nn.Conv2d(3, 16, 3, stride=2)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5, stride=2)
-        self.fc1 = nn.Linear(4560, 64)
-        self.fc2 = nn.Linear(64, 84)
-        self.fc3 = nn.Linear(84, 13)
+        self.conv2 = nn.Conv2d(16, 32, 3, stride=2)
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=2)
+        self.fc1 = nn.Linear(768, 768)
+        self.drop = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(768, 512)
+        self.fc3 = nn.Linear(512, 13)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
+        print(x.shape)
         x = F.relu(self.fc1(x))
+        x = self.drop(x)
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.drop(x)
+        x = F.softmax(self.fc3(x))
         return x
