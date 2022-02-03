2aa1efd6acff9e52f66efe44d459e3ade3d4d50f
Thije Robbe <thijer@hotmail>
2022-02-03 15:16:30 +0100

    PCA v0.2

--- dim-reduction-exp/pca.py
+++ dim-reduction-exp/pca.py
@@ -1,57 +1,68 @@
-from operator import index
 from sklearn.decomposition import IncrementalPCA
-from skimage.io import imsave, imread
+from skimage.io import imread
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+import pickle
 
 img_dim = 320 * 256 * 3
 path = "data/"
-batch_size = 2000
-components = 100
+batch_size = 10000
+components = None
 
 def load_img(id):
-    return imread(path + "normal/{}.jpg".format(id[0])).flatten()
+    return imread(path + "normal/{}.jpg".format(id[0])).astype(np.int16).flatten()
 
-def main():
-    
+def main(pca_path = None):
+    if(pca_path == None):
+        pca = IncrementalPCA(components, batch_size = batch_size)
+    else:
+        with open(pca_path, 'rb') as out:
+            pca = pickle.load(out)
+        
     data = pd.read_csv(path + "41K_processed_v3.csv", index_col='id')
     data = data[data["cols"] != 0]
+    ids = data.index.to_numpy()
+    ids = ids.reshape(ids.shape[0], 1)
+    
+    fit(pca, ids)
+    
 
-    rows = len(data)
-    # rows = 300
+def fit(pca: IncrementalPCA, ids):
+    rows = len(ids)
+    # rows = 1000
     batches = int(np.floor(rows / batch_size))
     rest = rows - batches * batch_size
 
     print("Rows: {}\tBatches: {}".format(rows, batches))
 
-    ids = data.index.to_numpy()
-    ids = ids.reshape(ids.shape[0], 1)
-    
-    # poster = imread(image_url)
-    # poster = np.reshape(poster, (1, poster.shape[0] * poster.shape[1] * poster.shape[2]))
-    pca = IncrementalPCA(components, batch_size = batch_size)
-
-    # imgs = np.zeros((batch_size, img_dim))
-    print("Start PCAing")
-    for i in range(0, batches*batch_size, batch_size):
-        ii = i % batch_size
+    print("Start fitting PCA")
+    for i in range(0, (batches - 1) * batch_size, batch_size):
         imgs = np.apply_along_axis(load_img, 1, ids[i : i + batch_size])
         # imgs[i] = imread(path + "normal\\{}.jpg".format()).flatten()
         print("Fitting {} of {}".format(int(i / batch_size) + 1, batches))
         pca.partial_fit(imgs)
     
     if(rest > 0): 
-        print("Fitting the rest")
-        imgs = np.apply_along_axis(load_img, 1, ids[batches*batch_size : batches*batch_size + rest])
-
-
+        print("Fitting {} of {}".format(int((i + 1) / batch_size) + 1, batches))
+        imgs = np.apply_along_axis(load_img, 1, ids[(batches - 1) * batch_size : (batches - 1) * batch_size + rest])
+    
     plt.grid()
-    plt.plot(pca.explained_variance_ratio_)
+    plt.title("Explained variance.")
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
     plt.xlabel('Number of components')
     plt.ylabel('Explained variance')
-    plt.savefig('Scree plot.png')
+    
+    if(components != None): 
+        plt.savefig('Scree_{}_components.png'.format(components))
+        with open("pca_{}.bin".format(components), 'wb') as out: pickle.dump(pca, out)
+    else: 
+        plt.savefig('Scree_all_components.png')
+        with open("pca.bin", 'wb') as out: pickle.dump(pca, out)
 
+
+def generate_pca_dataset(pca):
+    pass
 
 if __name__ == '__main__':
     """ 
