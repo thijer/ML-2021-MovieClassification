e4b35a7db3ea055c12b1943b73c0a66576fdab03
Thije Robbe <thijer@hotmail>
2022-02-06 17:03:56 +0100

    added figures folder

--- feature_extraction/pca.py
+++ feature_extraction/pca.py
@@ -5,17 +5,18 @@ import pandas as pd
 import numpy as np
 
 img_dim = 320 * 256 * 3
-path = "data/"
+path_data = "data/"
+path_figures = "figures/"
 batch_size = 1000
 components = None
 
 def load_img(id):
-    return imread(path + "normal/{}.jpg".format(id[0])).astype(np.int16).flatten()
+    return imread(path_data + "normal/{}.jpg".format(id[0])).astype(np.int16).flatten()
 
 def main(pca_path = None):
     pca = IncrementalPCA(components, batch_size = batch_size)
        
-    data = pd.read_csv(path + "41K_processed_v3.csv", index_col='id')
+    data = pd.read_csv(path_data + "41K_processed_v3.csv", index_col='id')
     data = data[data["cols"] != 0]
     ids = data.index.to_numpy()
     ids = ids.reshape(ids.shape[0], 1)
@@ -34,7 +35,7 @@ def fit(pca: IncrementalPCA, ids):
     print("Start fitting PCA")
     for i in range(0, (batches - 1) * batch_size, batch_size):
         imgs = np.apply_along_axis(load_img, 1, ids[i : i + batch_size])
-        # imgs[i] = imread(path + "normal\\{}.jpg".format()).flatten()
+        # imgs[i] = imread(path_data + "normal\\{}.jpg".format()).flatten()
         print("Fitting {} of {}".format(int(i / batch_size) + 1, batches))
         pca.partial_fit(imgs)
     
@@ -45,11 +46,11 @@ def fit(pca: IncrementalPCA, ids):
     plt.grid()
     plt.title("Explained variance.")
     plt.plot(np.cumsum(pca.explained_variance_ratio_))
-    plt.xlabel('Number of components')
-    plt.ylabel('Explained variance')
+    plt.xlabel("Number of components")
+    plt.ylabel("Explained variance")
     
-    if(components != None): plt.savefig('Scree_{}_components.png'.format(components))
-    else: plt.savefig('Scree_all_components.png')
+    if(components != None): plt.savefig(path_figures + "Scree_{}_components.png".format(components))
+    else: plt.savefig(path_figures + "Scree_all_components.png")
         
 def generate_pca_dataset(pca):
     pass
@@ -72,5 +73,5 @@ if __name__ == '__main__':
     #     exit()
     # components = sys.argv[2]
     # components = 1024
-    # path = os.getcwd() + "/data/duplicate_free_41K.csv"
+    # path_data = os.getcwd() + "/data/duplicate_free_41K.csv"
     main()
